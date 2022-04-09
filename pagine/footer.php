a02f99e13918dd4891a2e889e5077647838094fb
MartaBiellas <marta.biella@liceobanfi.eu>
2022-04-09 09:01:15 +0200

    II footer

--- pagine/footer.php
+++ pagine/footer.php
@@ -4,8 +4,8 @@
 
     echo '<div class="social col">';
     echo '<img src="immagni/icona-social-fb.svg" alt="">';
-    echo '<a href="https://www.instagram.com/agricola_celli/" target="_blank"><img src="immagni/icona-social-ig.svg"></a>'
-    echo '</div>'
+    echo '<a href="https://www.instagram.com/agricola_celli/" target="_blank"><img src="immagni/icona-social-ig.svg"></a>';
+    echo '</div>';
 
     echo '</div>';
 ?>
