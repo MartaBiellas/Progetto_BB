a546ffb934b7c1e370438ec6d4fd131fe5ccc784
MartaBiellas <marta.biella@liceobanfi.eu>
2022-04-09 08:58:42 +0200

    I modifica footer

--- pagine/footer.php
+++ pagine/footer.php
@@ -1,5 +1,11 @@
 <?php 
     echo '<div class="footer">';
-    echo '<em>"Biblioteca Online"</em>, esempio di sito dinamico in PHP realizzato da Francesco Tormene';
+    echo '<em>"Registro Online"</em>, esempio di sito dinamico in PHP realizzato da Francesco Tormene';
+
+    echo '<div class="social col">';
+    echo '<img src="immagni/icona-social-fb.svg" alt="">';
+    echo '<a href="https://www.instagram.com/agricola_celli/" target="_blank"><img src="immagni/icona-social-ig.svg"></a>'
+    echo '</div>'
+
     echo '</div>';
 ?>
