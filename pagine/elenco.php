09a121a33a9c699269d0ccd6309df8798eb674bb
ValeBresh <79479954+ValeBresh@users.noreply.github.com>
2022-04-22 20:25:54 +0200

    modifiche stile + elenco

--- pagine/elenco.php
+++ pagine/elenco.php
@@ -43,7 +43,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css" integrity="sha512-NhSC1YmyruXifcj/KFRWoC561YpHpc5Jtzgvbuzx5VozKpWvQ+4nXhPdFgmx8xqexRcpAglTj9sIBWINXa8x5w==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <title>Professore - Home Professore</title>
-	<link rel="stylesheet" type="text/css" href="../stilee.css">
+	<link rel="stylesheet" type="text/css" href="../stile.css">
 </head>
 <body>
 	<div class="nav">
@@ -75,29 +75,95 @@
 	
 	
 	<div class="contenuto">
-		<?php
-			$sql = "
-            SELECT *
-            FROM alunno
-            WHERE alunno.sezione= '$sezione' AND alunno.anno = '$anno'";
+		<div class="elenco_voti">
+        	<table id="tab_dati_personali">
+				
+				<?php
 
-			$ris = $conn->query($sql) or die("<p>Query fallita!</p>");
-           
-			while($riga = $ris->fetch_assoc()){
-				echo "<br><p><b>".$riga["nome"]." ".$riga["cognome"]."</b></p>";	
-			}
-			
-		?>
-		
+					$sql = "
+					SELECT *
+					FROM alunno
+					WHERE alunno.sezione= '$sezione' AND alunno.anno = '$anno'";
+
+					$dati_studenti = $conn->query($sql) or die("<p>Query fallita!</p>");
+					
+					$sql = "
+					SELECT *
+					FROM professore
+					WHERE professore.email = '$email'";
+
+					$dati_professore = $conn->query($sql) or die("<p>Query fallita!</p>");
+					
+					$dati_professore = $dati_professore->fetch_assoc();
+					
+					$incremento = 0; 
+					
+					while($riga = $dati_studenti->fetch_assoc()){
+					
+						$sql = "SELECT * 
+								FROM voto 
+								WHERE matricola_alunno ='$riga[matricola]' AND matricola_professore ='$dati_professore[matricola]'"; 
+						
+						$ris1 = $conn->query($sql) or die("<p>Query fallita!</p>");
+						
+						echo '<td>';
+						echo '<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+								<script>
+								$(document).ready(function(){
+									$("#menuButton'.$incremento.'").click(function(){
+									$("#menu'.$incremento.'").slideToggle();
+									}); 
+								});
+								</script>';
+														
+						$sql = "SELECT AVG(valutazione) AS media
+								FROM voto
+								WHERE matricola_alunno ='$riga[matricola]' AND matricola_professore ='$dati_professore[matricola]'";
+
+						$ris2 = $conn->query($sql) or die("<p>Query fallita!</p>");
+
+						$tmp = $ris2->fetch_assoc(); 
+						echo '<br>'; 
+						echo ''.$riga["nome"].' '.$riga["cognome"].' <button id="menuButton'.$incremento.'"  class = btn1> '.$tmp["media"].' </button>';
+						echo '<div id="menu'.$incremento.'" style="display:none;">';
+						if ($ris1->num_rows == 0) {
+							echo '<p> Nessuno </p>';
+						}else{
+							echo '<ul><br>';
+							while($riga = $ris1->fetch_assoc())
+							{
+								echo '<li>';
+								echo $riga["data"]." - ".$riga["tipo"].":<b> ".$riga["valutazione"]."</b>";
+								echo '</li>';
+							}
+							echo '</ul>';
+						}
+						echo '</td>';
+						echo '</tr>';
+						
+						$incremento = $incremento + 1;
+						
+						$ris1 = null;
+						$ris2 = null;
+						echo "</div>";    	
+					}
+					
+				?>
+
+			</table>
+		</div>
 	</div>
+	
 	<br>
 	<br>
 	
 	<?php 
 		include('footer.php')
 	?>
+
 </body>
 </html>
+
 <?php
 	$conn->close();
 ?>
